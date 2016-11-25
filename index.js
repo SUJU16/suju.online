5c259f8e5bf5fc8271f7f9b08e43fc75418f7427
Jere Vaara <jerevaara@gmail.com>
2016-11-25 17:52:10 +0200

    es6 stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
+import express from 'express'
 
-app.get('/', (req, res) => res.send('Hello Junction'));
-app.listen(5000, () => console.log('server running on 5000'));
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello Junction'))
+app.listen(5000, () => console.log('server running on 5000'))
