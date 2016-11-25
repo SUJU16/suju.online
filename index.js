24f3360dcce02e6e31680d907fbc151bd429e08b
Laged <parkkila.matti@gmail.com>
2016-11-25 19:31:25 +0200

    Merge to database implementation

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
-
+import massive from 'massive'
 const app = express()
+const connectionString = process.env.DATABASE_URL
+const db = massive.connectSync({connectionString : connectionString})
+app.set('db', db)
 
 app
 .use(express.static('./dist'))
-// .get('/', (req, res) => res.send('Hello Junction'))
+.get('/database', (req, res) => {
+  db.requests.find({}, (err, result) => {
+    if (err) res.status(500).json({error:err})
+    res.json(result)
+  })
+})
 .listen(5000, () => console.log('server running on 5000'))
