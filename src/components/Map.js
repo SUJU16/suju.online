fa5b889c0e5e01cf082bdc079afe4fc11671f0de
Jere Vaara <jerevaara@gmail.com>
2016-11-26 13:31:47 +0200

    datapoints on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,29 +5,33 @@ import { setLocation } from '../store/actions'
 import Page from './Page'
 import style from '../styles/Map.scss'
 
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, LayerGroup, Circle } from 'react-leaflet';
 
-const LeafLet = (location, zoomLevel) => {
-  return (
-    <Map className={style.map} center={location} zoom={zoomLevel}>
-      <TileLayer
-        className={style.tileLayer}
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url='http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
-        />
-    </Map>
-  )
-}
-
-const MapView = ({ location, zoomLevel, setLocation }) => (
+const MapView = ({ datapoints, location, zoomLevel, setLocation }) => (
   <div className={style.container}>
   {location ?
-    LeafLet(location, zoomLevel)
+    <Map className={style.map} center={location} zoom={zoomLevel}>
+      <LayerGroup>
+        <TileLayer
+          className={style.tileLayer}
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+          />
+        { datapoints ? datapoints.map(point => {
+          const coords = {
+            lat: point.latitude,
+            lon: point.longitude
+          }
+          return (<Circle key={point.id} center={coords} radius={20} />)
+        }) : null}
+      </LayerGroup>
+    </Map>
     : 'Loading location'}
   </div>
 )
 
 const mapStateToProps = (state) => ({
+  datapoints: state.clusters.datapoints,
   location: state.preferences.location,
   zoomLevel: state.preferences.zoomLevel
 })
