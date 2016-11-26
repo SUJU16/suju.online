3e5367285dbbdc7776e1628f8e33ee2f9a388b2e
Laged <parkkila.matti@gmail.com>
2016-11-26 22:29:02 +0200

    Refactor promise chaining

--- routes/database.js
+++ routes/database.js
@@ -5,21 +5,13 @@ import jsonHelper from '../utils/jsonHelper'
 export default express.Router()
 .get('/', (req, res) => {
   db.getData()
-  .then( (response) => {
-    res.json(response)
-  })
-  .catch( (error) => {
-    res.status(500).json({err: error})
-  })
+  .then( (response) => res.json(response))
+  .catch( (error) => res.status(500).json({err: error}))
 })
 .get('/addRandom', (req, res) => {
   db.postRequest(jsonHelper.generateRandomJSON())
-  .then( (response) => {
-    res.json(response)
-  })
-  .catch( (error) => {
-    res.status(500).json({err:error})
-  })
+  .then( (response) => res.json(response))
+  .catch( (error) => res.status(500).json({err:error}))
 })
 .get('/fillTest/:amount', (req, res) => {
   for(var i = 0; i < req.params.amount; i++) {
@@ -29,19 +21,11 @@ export default express.Router()
 })
 .get('/removeData', (req, res) => {
   db.removeData()
-  .then( (response) => {
-    res.json(response)
-  })
-  .catch( (error) => {
-    res.status(500).json({err: error})
-  })
+  .then( (response) => res.json(response))
+  .catch( (error) => res.status(500).json({err: error}))
 })
 .post('/', (req, res) => {
   db.postRequest(req.body)
-  .then( (response) => {
-    res.json(response);
-  })
-  .catch( (error) => {
-    res.status(500).json({err: error})
-  })
+  .then( (response) => res.json(response))
+  .catch( (error) => res.status(500).json({err: error}))
 })
