523e66ae71c757604f7a231942588603d05cf2c3
Jere Vaara <jerevaara@gmail.com>
2016-11-26 21:30:55 +0200

    polyline tools

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+import polyline from 'polyline'
+import routes from './routes'
+
+const readRoute = (raw) => {
+  const coords = polyline.decode(raw.route, 6)
+  return coords
+}
+
+export const getCoordinates = () => {
+  console.log('getCoordinates')
+  return routes.map(list => readRoute(list[0]))
+}
